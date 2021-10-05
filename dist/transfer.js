330bb76cb08b6219847b5bf703fcb73032b18715
ibn5x <87243859+ibn5x@users.noreply.github.com>
2021-10-05 01:56:52 -0400

    implemented transfering of assets and enjimon

---
+++ dist/transfer.js
@@ -0,0 +1,41 @@
+Moralis.initialize("xXH1IL0ORWvCjl2vMfMmi1EpwhIrXE17mQjYJCRN"); // Application id from moralis.io
+Moralis.serverURL = "https://bpsygtsl1obp.moralishost.com:2053/server"; //Server url from moralis.io
+const CONTRACT_ADDRESS = "0x19A92f37e090a346cA5D226a5aa381035949dCdA";
+let currentUser;
+let web3;
+
+init = async () =>{
+    currentUser = await Moralis.User.current();
+
+    if(!currentUser){
+        window.location.pathname = "./index.html"; 
+    }
+    
+    web3 = await Moralis.Web3.enable();
+    const urlParams = new URLSearchParams(window.location.search);
+    const nftId = urlParams.get("nftId"); 
+   
+    document.getElementById('token_id_input').value = nftId; //prepopulate id
+    
+}
+
+transfer = async () => {
+    
+    let tokenId = parseInt(document.getElementById('token_id_input').value);
+    let address = document.getElementById('address_input').value;
+    let amount = parseInt(document.getElementById('amount_input').value);
+    
+    // sending 15 tokens with token id = 1
+    const options = {type: "erc1155",  
+    receiver: address,
+    contract_address: CONTRACT_ADDRESS,
+    token_id: tokenId,
+    amount: amount}
+    
+    let result = await Moralis.transfer(options)
+    console.log(result);
+}
+
+document.getElementById('submit_transfer').onclick = transfer;
+
+init();
